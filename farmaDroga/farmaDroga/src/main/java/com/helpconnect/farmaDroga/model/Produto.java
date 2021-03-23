909cd92d75cabe2bc042c2a920fd4f050a2c9bc8
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-03-23 12:58:25 -0300

    Criacao de relacionamento Classe Categoria (1:N), Classe Produto (N:1)

--- farmaDroga/farmaDroga/src/main/java/com/helpconnect/farmaDroga/model/Produto.java
+++ farmaDroga/farmaDroga/src/main/java/com/helpconnect/farmaDroga/model/Produto.java
@@ -4,11 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "produto")
@@ -38,6 +41,10 @@ public class Produto {
 	
 	@NotNull
 	private boolean ativo;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 	
 	public long getId() {
 		return id;
@@ -94,4 +101,13 @@ public class Produto {
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
 	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
 }
