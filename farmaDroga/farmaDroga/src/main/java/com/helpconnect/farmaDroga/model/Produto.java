a47c32f551ba3f644aede85a2addd4cb66d60067
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-03-23 12:34:15 -0300

    Criacao de classe produto, produtoRepository e produtoController e implementacoes de funcoes para manipulacao de dados utilizando o CRUD desenvolvido dentro da ProdutoController

---
+++ farmaDroga/farmaDroga/src/main/java/com/helpconnect/farmaDroga/model/Produto.java
@@ -0,0 +1,97 @@
+package com.helpconnect.farmaDroga.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(max = 255)
+	private String nome;
+	
+	@NotNull
+	@Size(max = 1000)
+	private String descricao;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	@Size(max = 500)
+	private String img;
+	
+	@NotNull
+	private int qtdProduto;
+	
+	@NotNull
+	private boolean ativo;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public String getImg() {
+		return img;
+	}
+	
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	public int getQtdProduto() {
+		return qtdProduto;
+	}
+	
+	public void setQtdProduto(int qtdProduto) {
+		this.qtdProduto = qtdProduto;
+	}
+	
+	public boolean isAtivo() {
+		return ativo;
+	}
+	
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+}
---
+++ farmaDroga/farmaDroga/src/main/java/com/helpconnect/farmaDroga/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.helpconnect.farmaDroga.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.helpconnect.farmaDroga.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	
+	public List<Produto> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
