78282944d93a64d71f82e9f8d4205559f89f837f
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-03-23 09:57:23 -0300

    Criacao de projeto Spring referente a um sistema de farmacia

---
+++ farmaDroga/farmaDroga/src/main/java/com/helpconnect/farmaDroga/FarmaDrogaApplication.java
@@ -0,0 +1,13 @@
+package com.helpconnect.farmaDroga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaDrogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaDrogaApplication.class, args);
+	}
+
+}
